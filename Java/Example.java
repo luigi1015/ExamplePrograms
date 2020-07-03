cbfdd3ce22d3b4a6d4ce5571102ff3050caaa7b4
Jeff <github@314ghz.com>
2020-07-03 06:49:27 -0500

    Modified the application to be a simple Java Swing application.

--- Java/Example.java
+++ Java/Example.java
@@ -1,6 +1,35 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Dimension;
+import javax.swing.SwingUtilities;
+
 public class Example
 {
+	private static void createGUI()
+	{
+		// Create the window
+		JFrame exampleFrame = new JFrame("Example Frame");
+		exampleFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		exampleFrame.setPreferredSize(new Dimension(400, 300));
+
+		// Create a Hello World label
+		JLabel helloWorldLabel = new JLabel("Hello World");
+		exampleFrame.getContentPane().add(helloWorldLabel);
+
+		// Display the window
+		exampleFrame.pack();
+		exampleFrame.setLocationRelativeTo(null);
+		exampleFrame.setVisible(true);
+	}
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World Start");
+
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				createGUI();
+			}
+		});
 	}
 }
